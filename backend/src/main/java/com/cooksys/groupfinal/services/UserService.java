d7886bc5772e10fae053fbe74495a517a5ba062a
Kevin Dyer <kpdyer07@gmail.com>
2024-04-22 15:07:45 -0700

    feat: PATCH users/{id}
    currently only need to be able to change the admin status of a user, but we may
    implement patches for any field in the future.

--- backend/src/main/java/com/cooksys/groupfinal/services/UserService.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/UserService.java
@@ -1,11 +1,12 @@
 package com.cooksys.groupfinal.services;
 
+import com.cooksys.groupfinal.dtos.BasicUserDto;
 import com.cooksys.groupfinal.dtos.CredentialsDto;
 import com.cooksys.groupfinal.dtos.FullUserDto;
 
 public interface UserService {
 
 	FullUserDto login(CredentialsDto credentialsDto);
-
+	BasicUserDto changeUserAdminStatus(Long id, boolean adminStatus);
    
 }
--- backend/src/main/java/com/cooksys/groupfinal/services/impl/UserServiceImpl.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/impl/UserServiceImpl.java
@@ -4,13 +4,16 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.cooksys.groupfinal.dtos.BasicUserDto;
 import com.cooksys.groupfinal.dtos.CredentialsDto;
 import com.cooksys.groupfinal.dtos.FullUserDto;
+import com.cooksys.groupfinal.dtos.UserRequestDto;
 import com.cooksys.groupfinal.entities.Credentials;
 import com.cooksys.groupfinal.entities.User;
 import com.cooksys.groupfinal.exceptions.BadRequestException;
 import com.cooksys.groupfinal.exceptions.NotAuthorizedException;
 import com.cooksys.groupfinal.exceptions.NotFoundException;
+import com.cooksys.groupfinal.mappers.BasicUserMapper;
 import com.cooksys.groupfinal.mappers.CredentialsMapper;
 import com.cooksys.groupfinal.mappers.FullUserMapper;
 import com.cooksys.groupfinal.repositories.UserRepository;
@@ -23,13 +26,22 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImpl implements UserService {
 	
 	private final UserRepository userRepository;
-  private final FullUserMapper fullUserMapper;
+    private final FullUserMapper fullUserMapper;
+    private final BasicUserMapper basicUserMapper;
 	private final CredentialsMapper credentialsMapper;
 	
 	private User findUser(String username) {
         Optional<User> user = userRepository.findByCredentialsUsernameAndActiveTrue(username);
         if (user.isEmpty()) {
             throw new NotFoundException("The username provided does not belong to an active user.");
+        }
+        return user.get();
+    }
+
+    private User findUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new NotFoundException("The id provided does not belong to an active user.");
         }
         return user.get();
     }
@@ -50,6 +62,13 @@ public class UserServiceImpl implements UserService {
         }
         return fullUserMapper.entityToFullUserDto(userToValidate);
 	}
+
+    @Override
+    public BasicUserDto changeUserAdminStatus(Long id, boolean adminStatus) {
+        User user = findUserById(id);
+        user.setAdmin(adminStatus);
+        return basicUserMapper.entityToBasicUserDto(userRepository.saveAndFlush(user));
+    }
 	
 	
 	
