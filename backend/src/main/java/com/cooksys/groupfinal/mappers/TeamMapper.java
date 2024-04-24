57edd0f72a83550a6864ac76cae1f70f9ae62231
Kevin Dyer <kpdyer07@gmail.com>
2024-04-23 17:28:13 -0700

    feat: add project count to teamdto

--- backend/src/main/java/com/cooksys/groupfinal/mappers/TeamMapper.java
+++ backend/src/main/java/com/cooksys/groupfinal/mappers/TeamMapper.java
@@ -2,7 +2,9 @@ package com.cooksys.groupfinal.mappers;
 
 import java.util.Set;
 
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import com.cooksys.groupfinal.dtos.TeamDto;
 import com.cooksys.groupfinal.entities.Team;
@@ -13,5 +15,10 @@ public interface TeamMapper {
 	TeamDto entityToDto(Team team);
 
   Set<TeamDto> entitiesToDtos(Set<Team> teams);
+
+  @AfterMapping
+  default void setNumberOfProjects(Team team, @MappingTarget TeamDto teamDto) {
+    teamDto.setNumberOfProjects(team.getProjects().size());
+  }
 
 }
